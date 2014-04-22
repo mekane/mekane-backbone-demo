94de6859f63f1d5e2909f800b67d5175961404b4
Marty Kane <marty@pixotech.com>
2014-04-22 10:31:31 -0500

    Use resetView to filter employees based on route

--- routes.js
+++ routes.js
@@ -17,14 +17,17 @@ var app = app || {};
 
         showAll: function(){
             console.log("SHOW ALL");
+            app.employeeList.resetViews();
         },
 
         filterByRole: function( role ){
             console.log("SHOW ALL "+role+"s");
+            app.employeeList.resetViews( employeeList.where({role:role}) );
         },
 
         filterByDrink: function( drink ){
             console.log("SHOW ALL "+drink+" drinkers");
+            employeeList.resetViews( employeeList.where({drink:drink}) );
         }
     
     });
